e7f5647fa3d222254f0422fdb122e7d11afe13c4
Németh Dániel <nem.dani0627@gmail.com>
2021-01-10 11:52:01 +0100

    Deleting useless codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import Customers from './components/Customers';
-import About from "./components/About";
-import Home from "./components/Home";
-import Nav from "./components/Nav";
-import Tervek from "./components/Tervek";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./css/main.css";
-import "./css/loading.css";
-import "./css/popup.css";
 
 
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/customer" component={Customers} />
-        <Route path="/about" component={About} />
-        <Route path="/files/:fileID" component={Tervek} />
-      </Switch>
+    <h1>Hello world!</h1>
+    // <Router>
+    //   <Nav />
+    //   <Switch>
+    //     <Route path="/" exact component={Home} />
+    //     <Route path="/customer" component={Customers} />
+    //     <Route path="/about" component={About} />
+    //     <Route path="/files/:fileID" component={Tervek} />
+    //   </Switch>
 
-    </Router>
+    // </Router>
   );
 }
 
